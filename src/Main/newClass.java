3d4eac9d4e751961689e225aac80f656a5f80a97
rekebas <aekebas@hotmail.com>
2020-10-29 14:06:00 -0500

    Third

--- src/Main/newClass.java
+++ src/Main/newClass.java
@@ -6,6 +6,7 @@ public class newClass {
     public static void main(String[] args) {
 
         System.out.println(StrReverse("Richard"));
+        System.out.println(StrReverse("Ekebas"));
     }
 
 
