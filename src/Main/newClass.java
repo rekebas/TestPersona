5535c9179aa36e019689968b8f0db6000727b883
rekebas <aekebas@hotmail.com>
2020-10-29 13:54:03 -0500

    first commit

---
+++ src/Main/newClass.java
@@ -0,0 +1,26 @@
+package Main;
+
+public class newClass {
+
+
+    public static void main(String[] args) {
+
+        System.out.println(StrReverse("Richard"));
+    }
+
+
+    public static String StrReverse(String str) {
+
+        String reverse="";
+
+        for(int i=str.length()-1; i >= 0; i--)
+
+            reverse += str.toCharArray()[i];
+
+
+
+        return  reverse;
+
+    }
+
+}
